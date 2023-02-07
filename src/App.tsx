0f0c357a8608d6045bded783d39fd4b3bb76f345
FernandoCalvitti <83254261+FernandoCalvitti@users.noreply.github.com>
2023-02-07 19:00:53 -0300

    Added react-router-dom to the project (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Container } from "@mui/material";
 import * as React from "react";
 import Layout from "./components/Layout";
 import Home from "./Pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import { CART, HOME } from "./config/Routes";
+import CartPage from "./Pages/CartPage";
 
 export default function App() {
   return (
     <Layout>
-      <Home />
+      <Routes>
+        <Route path={HOME} element={<Home />} />
+        <Route path={CART} element={<CartPage />} />
+      </Routes>
     </Layout>
   );
 }
