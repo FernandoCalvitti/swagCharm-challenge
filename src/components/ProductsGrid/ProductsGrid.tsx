a6ec562c7fa58dc9aca0aa9e49dbb7d314798ee1
FernandoCalvitti <83254261+FernandoCalvitti@users.noreply.github.com>
2023-02-10 17:15:27 -0300

    Added basic fetch and show of products (#4)

--- src/components/ProductsGrid/ProductsGrid.tsx
+++ src/components/ProductsGrid/ProductsGrid.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Card, Container, Grid } from "@mui/material";
+import { Stack } from "@mui/system";
+
+import Styles from "./ProductsGrid.module.css";
+import Product from "../Product/Product";
 
 type Props = {};
 
+type Rating = {
+  rate: number;
+  count: number;
+};
+
+export type Item = {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: Rating;
+  title: string;
+};
+
 const ProductsGrid = (props: Props) => {
-  return <div>ProductsGrid</div>;
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    try {
+      fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((data) => setItems(data));
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  console.log(items);
+  return (
+    <main className={Styles.gridContainer}>
+      {items.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </main>
+  );
 };
 
 export default ProductsGrid;
